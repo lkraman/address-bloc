0ae05025b071df4e311d8375627313fde4da9698
lkraman <laurakraman01@gmail.com>
2019-05-04 20:10:32 -0400

    committing work for 5/4/2019. Incomplete checkpoint. Resume where code tested resulted in hello clear/hello from main

---
+++ controllers/MenuController.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+
+ module.exports = class MenuController {
+   constructor(){}
+ 
+
+ main(){
+     console.log('hello from main');
+   }
+
+   clear(){
+     console.log('hello from clear');
+   }
+}
