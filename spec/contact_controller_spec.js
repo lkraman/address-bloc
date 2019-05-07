a54ce680454beb7819c9c2744443f39293d64eb2
lkraman <laurakraman01@gmail.com>
2019-05-07 18:28:21 -0400

    commit changes

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -20,11 +20,12 @@ const sequelize = require("../db/models/index").sequelize;
    describe("#addContact()", () => {
 
       it("should add a single contact into the book", (done) => {
-        this.book.addContact("Alice", "001-101-1010")
+        this.book.addContact("Alice", "001-101-1010", "alice@gmail.com")
           .then((contact) => {
 
             expect(contact.name).toBe("Alice");
             expect(contact.phone).toBe("001-101-1010");
+            expect(contact.name).toBe("alice@gmail.com");
             done();
           })
           .catch((err) => {
